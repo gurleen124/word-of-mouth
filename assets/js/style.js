136c11744f61d6a689f783605a79bdf8c64d0bc1
Gurleen Kaur <gurleen.kaur@lpinfotech.com>
2024-03-06 13:02:32 +0530

    profile page created

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,11 +2,11 @@
 $(document).ready(function () {
     $("body").addClass('default');
     // nav link active
-    $(".btn").click(function () {
-        $('.btn').removeClass("active");
+    $(".theme").click(function () {
+        $('.theme').removeClass("active");
         $(this).addClass("active");
         // body class change
-        var text = $(".btn.active").find(".theme-text").html();
+        var text = $(".theme.active").find(".theme-text").html();
         $('body').removeClass();
         $('body').addClass(text);
     });
